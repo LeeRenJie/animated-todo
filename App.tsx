c05b3242db08f99e10b551a217c8f6131baa7eb3
Lee Ren Jie <work.renjie@gmail.com>
2023-06-08 23:50:53 +0800

    completed code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import AppContainer from './src/components/app-container';
-import Main from './src/screens/main'
+import React from 'react'
+import AppContainer from './src/components/app-container'
+import Navigator from './src/'
 
 export default function App() {
   return (
     <AppContainer>
-      <Main/>
+      <Navigator />
     </AppContainer>
-  );
+  )
 }
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -3,16 +3,14 @@ import { Text, HStack, Switch, useColorMode } from 'native-base'
 
 export default function ThemeToggle() {
   const { colorMode, toggleColorMode } = useColorMode()
-
   return (
     <HStack space={2} alignItems="center">
       <Text>Dark</Text>
-      {/* Switch component to toggle between light and dark mode */}
       <Switch
-        isChecked={colorMode === 'light'} // Check if the current mode is light
-        onToggle={toggleColorMode} // Function to toggle the color mode
+        isChecked={colorMode === 'light'}
+        onToggle={toggleColorMode}
       ></Switch>
       <Text>Light</Text>
     </HStack>
-  );
+  )
 }
