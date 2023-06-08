fc79324d405125963280159180a1318e3bbd1507
Lee Ren Jie <work.renjie@gmail.com>
2023-06-08 11:28:48 +0800

    feat: add animated checkbox

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -5,14 +5,12 @@ import theme from '../theme'
 
 type Props = {
   children: React.ReactNode
-} // Defining the 'Props' type that expects a 'children' prop of type 'React.ReactNode'
+}
 
 
-export default function AppContainer(props: Props){ // Defining a default function component 'AppContainer' that accepts 'props' of type 'Props'
-  return ( // Returning the following JSX code
-    // Using the 'NavigationContainer' component as a container for navigation functionality
+export default function AppContainer(props: Props){
+  return (
     <NavigationContainer>
-      {/* Using the 'NativeBaseProvider' component as a container for rendering the app's UI, with the 'theme' object passed as a prop, and rendering the children passed to the 'AppContainer' component */}
       <NativeBaseProvider theme={theme}>{props.children}</NativeBaseProvider>
     </NavigationContainer>
   )
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react'
 import { Text, Box, Center, VStack, useColorModeValue } from "native-base"
 import ThemeToggle from "../components/theme-toggle"
+import AnimatedCheckbox from '../components/animated-checkbox'
 
 export default function MainScreen() {
   return (
     <Center
-      _dark={{ bg: 'blueGray.900' }} // Dark mode background color
-      _light={{ bg: 'blueGray.50' }} // Light mode background color
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'blueGray.50' }}
       px={4}
       flex={1}
     >
       <VStack space={5} alignItems="center">
-        <Box p="10" bg={useColorModeValue('red.500', 'yellow.500')}>
-          <Text color={useColorModeValue('white', 'black')}>Hello</Text> {/* Text color depending on the color mode */}
+        <Box w="100px" h="100px">
+          <AnimatedCheckbox/>
         </Box>
-        <ThemeToggle /> {/* Theme toggle component */}
+        <Box p="10" bg={useColorModeValue('red.500', 'yellow.500')}>
+          <Text color={useColorModeValue('white', 'black')}>Hello</Text>
+        </Box>
+        <ThemeToggle />
       </VStack>
     </Center>
   )
